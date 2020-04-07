5d21daf734dd4582e239d848821f1d7b5f8d465d
Nico Yeager <nicoyeager@gmail.com>
2020-04-07 18:35:45 -0400

    crt collect view and link var

---
+++ Coder-Swag/Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Nico Yeager on 4/7/20.
+//  Copyright © 2020 Nico Yeager. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
