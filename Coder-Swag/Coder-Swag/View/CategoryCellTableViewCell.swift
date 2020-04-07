d5eba9495c68b9c31ea1f4428745e7f5594691aa
Nico Yeager <nicoyeager@gmail.com>
2020-04-07 15:33:04 -0400

    finished less1 - create tblview

---
+++ Coder-Swag/Coder-Swag/View/CategoryCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCellTableViewCell.swift
+//  Coder-Swag
+//
+//  Created by Nico Yeager on 4/7/20.
+//  Copyright © 2020 Nico Yeager. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
